8f4e7da1861285fe1d3946bf872ea53f806aae53
Sbordi <110256170+Sbord1@users.noreply.github.com>
2023-06-12 00:00:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,6 @@
 <?php
 
-  ini_set('display_errors', 1);
-  error_reporting(E_ALL);
-
-  // dati sul database e le tabelle (magari messi in un file a parte ...)
-  $db_name = "VideotecaOnlinedb";
-  $VOuser_table = "VOuser";
-  $VOmovie_table = "VOmovie";
-
-  // mysqli: oggetto la cui creazione corrisponde all'effettuazione della connessione al database
-  // localhost e' l'host su cui gira il dbms MySQL;
-  // riccardo, con password password e' un esempio di utente mysql (registrato sul dbms con la possibilita' di accedere ad alcune basi di dati).
-  $mysqliConnection = new mysqli("localhost", "riccardo", "password", $db_name);
-
-  // Controllo della connessione
-  // mysqli_errno() and mysqli_error() restituiscono codici e messaggi relativi all'ultimo errore
-  if (mysqli_connect_errno()) {
-      printf("Errore! Problemi con la connessione al db: %s\n", mysqli_connect_error());
-      exit();
-  }
+	require_once("./connection.php");
 
   // Una volta che siamo nel db, verichiamo cosa e' stato passato come username e pwd e facciamo una query per controllare
   if (isset($_POST['invio'])) // abbiamo appena inviato dati attraverso la form di login
